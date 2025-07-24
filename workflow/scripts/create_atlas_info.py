b68d1156a9640f2c4c92434e198273a3d62cbccf
daviddaubner42 <davidaubner@gmail.com>
2025-07-24 17:32:32 +0200

    Set up the pipeline with conversion to cifti

---
+++ workflow/scripts/create_atlas_info.py
@@ -0,0 +1,21 @@
+import nibabel as nib
+import pandas as pd
+import argparse
+
+parser = argparse.ArgumentParser(description="Create remap from label file.")
+parser.add_argument("-atlas_file", type=str, help="Path to the label GIFTI file")
+parser.add_argument("-output", type=str, help="Path to the label GIFTI file")
+args = parser.parse_args()
+
+atlas = nib.load(args.atlas_file)
+axes = [atlas.header.get_axis(i) for i in range(atlas.ndim)]
+
+label_dict = axes[0].label[0]
+
+label_df = pd.DataFrame({"index": [], "label": []})
+
+for i, t in label_dict.items():
+    if i > 1:
+        label_df.loc[i-2] = [i-1, t[0]]
+
+label_df.to_csv(args.output, sep='\t', index=False)
---
+++ workflow/scripts/create_remap.py
@@ -0,0 +1,15 @@
+import nibabel as nib
+import argparse
+
+parser = argparse.ArgumentParser(description="Create remap from label file.")
+parser.add_argument("-input", type=str, help="Path to the label GIFTI file")
+parser.add_argument("-output", type=str, help="Path to the label GIFTI file")
+args = parser.parse_args()
+
+rh = nib.load(args.filename)
+
+og_keys = rh.labeltable.get_labels_as_dict().keys()
+
+with open(args.output, 'w') as f:
+    for key in og_keys:
+        f.write(f"{key} {key+len(og_keys)}\n")
